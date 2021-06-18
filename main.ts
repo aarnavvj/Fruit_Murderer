f01295bd798e9a1775c30e6f25bbc185e37bddb1
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-06-18 19:47:55 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 basic.forever(function () {
+    // This code helps food make their feelings clear about being eaten.
     if (input.pinIsPressed(TouchPin.P1)) {
         basic.showLeds(`
             # . . . #
